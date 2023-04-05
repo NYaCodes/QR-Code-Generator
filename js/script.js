d6efd793be41f149f9c5e8ea0631c01480b347f3
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-04-05 14:21:17 -0500

    Updated Files to fix qr code image

--- js/script.js
+++ js/script.js
@@ -5,29 +5,76 @@ const qr = document.getElementById('qrcode');
 const onGenerateSubmit = (e) => {
   e.preventDefault();
 
-
+  clearUI();
 
   const url = document.getElementById('url').value;
   const size = document.getElementById('size').value;
 
-if (url ===''){
+  // Validate url
+  if (url === '') {
     alert('Please enter a URL');
-} else {
-showSpinner();
-setTimeout( () =>{ 
-hideSpinner();
-}, 1000)
-}
-}
+  } else {
+    showSpinner();
+    // Show spinner for 1 sec
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(url, size);
+
+      // Generate the save button after the qr code image src is ready
+      setTimeout(() => {
+        // Get save url
+        const saveUrl = qr.querySelector('img').src;
+        // Create save button
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
+};
+console.log(onGenerateSubmit);
+
+// Generate QR code
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    width: size,
+    height: size,
+   
+  });
+};
+
+// Clear QR code and save button
+const clearUI = () => {
+  qr.innerHTML = '';
+  const saveBtn = document.getElementById('save-link');
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+// Show spinner
 const showSpinner = () => {
-    document.getElementById('spinner').style.display = 'block';
-}
+  const spinner = document.getElementById('spinner');
+  spinner.style.display = 'block';
+};
+
+// Hide spinner
 const hideSpinner = () => {
-    document.getElementById('spinner').style.display = 'none';
-}
-hideSpinner()
+  const spinner = document.getElementById('spinner');
+  spinner.style.display = 'none';
+};
 
+// Create save button to download QR code as image
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList =
+    'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = 'Save Image';
+  document.getElementById('generated').appendChild(link);
+};
 
+hideSpinner();
 
-
-form.addEventListener('submit', onGenerateSubmit);
+form.addEventListener('submit', onGenerateSubmit)
